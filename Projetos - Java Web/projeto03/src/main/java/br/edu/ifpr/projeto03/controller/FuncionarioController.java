c236ff40505951e71739a4c77760c0bc2455b52f
Vinicius Kahler Mordhorst <99055726+ViniciusMordhorst@users.noreply.github.com>
2024-09-13 18:43:44 -0300

    Delete Projetos - Java Web directory

--- Projetos - Java Web/projeto03/src/main/java/br/edu/ifpr/projeto03/controller/FuncionarioController.java
+++
@@ -1,5 +0,0 @@
-package br.edu.ifpr.projeto03.controller;
-
-public class FuncionarioController {
-    
-}
--- Projetos - Java Web/projeto03/src/main/java/br/edu/ifpr/projeto03/repositorio/SetorRepositorio.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifpr.projeto03.repositorio;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.edu.ifpr.projeto03.model.Setor;
-
-public interface SetorRepositorio extends CrudRepository<Setor, Long> {
-    
-}
